e08c0e88a3cc379eeaf9fcd3bf1cf135710d622f
platypuswithoutk <roksana.kurasinska@gmail.com>
2021-08-30 16:20:20 +0200

    change bootstrap grid in header

--- wp-content/themes/mythemes/header.php
+++ wp-content/themes/mythemes/header.php
@@ -24,7 +24,7 @@
 
         <div class="container">
             <div class="row" id="menu_style">
-                <div class="col-xl-3 col-lg-3 col-md-3 col-xs-6 col-12">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 col-12">
                     <div class="logo">
                         <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-removebg-preview.png'; ?>" alt="logo"></a>
                     </div>
