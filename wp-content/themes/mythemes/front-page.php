61cea978ce3638bad79b03ea16ed43c1f269c180
platypuswithoutk <roksana.kurasinska@gmail.com>
2021-09-09 22:31:47 +0200

    add description and title in custom filds

--- wp-content/themes/mythemes/front-page.php
+++ wp-content/themes/mythemes/front-page.php
@@ -13,17 +13,17 @@
     <div class="row" id="front-page-introduction">
       <div class="col-xl-6 col-lg-6 col-md-12 col-12" id="col-introdution">
         <div class="about-box">
-          <h2 class="up-text">Cześć! Nazywam się<?php the_field('firstdescription');?></h2>
+          <h2 class="up-text"><?php the_field('firstdescription');?></h2>
           <h1><span>R</span>oksana <span>D</span>zioba</h1>
-          <h2><?php the_field('main_description');?>i dziękuję za odwiedziny mojej strony. Jestem szybko i chętnie rozwijającym się Front-end Developerem z Krakowa. Jeżeli jesteś zainteresowany, poznaj mnie lepiej </h2>
+          <h2><?php the_field('main_description');?></h2>
           <div class="box"></div>
         
           <div class="buttons top">
             <button class="button type3">
-              <a href="<?php the_field('button1');?>" class="btn btn-3">O mnie<?php the_field('button1_desc');?></a>
+              <a href="<?php the_field('button1');?>" class="btn btn-3"><?php the_field('button1_desc');?></a>
             </button>
             <button class="button type3">
-              <a href="<?php the_field('button2');?>" class="btn btn-3">Moje projekty<?php the_field('button2_desc');?></a>
+              <a href="<?php the_field('button2');?>" class="btn btn-3"><?php the_field('button2_desc');?></a>
             </button>
           </div>
         </div>
@@ -32,7 +32,6 @@
       <div class="col-xl-6 col-lg-6 col-md-12 col-12">
         <div class="front-page_img">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/sectionIMG.jpg'; ?>" alt="logo">
-          
         </div>
       </div> 
     </div>
@@ -70,13 +69,13 @@
       </div>
       <div class="buttons anchors">
           <button class="button type3 link">
-            <a href="<?php the_field('button3');?>" class="btn btn-3">Zobacz więcej<?php the_field('button3_desc');?></a>
+            <a href="<?php the_field('button3');?>" class="btn btn-3"><?php the_field('button3_desc');?></a>
           </button>
           <button class="button type3 link">
-            <a href="<?php the_field('button4');?>" class="btn btn-3">Moje doświadczenie<?php the_field('button4_desc');?></a>
+            <a href="<?php the_field('button4');?>" class="btn btn-3"><?php the_field('button4_desc');?></a>
           </button>
           <button class="button type3 link">
-            <a href="<?php the_field('button5');?>" class="btn btn-3">Kontakt<?php the_field('button5_desc');?></a>
+            <a href="<?php the_field('button5');?>" class="btn btn-3"><?php the_field('button5_desc');?></a>
           </button>
       </div>
       </div>
