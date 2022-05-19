77907bd9e33bb86c22682709e2379a6dceaa7585
platypuswithoutk <roksana.kurasinska@gmail.com>
2022-05-19 21:24:49 +0200

    Add new projects

--- wp-content/themes/mytheme/page-projects.php
+++ wp-content/themes/mytheme/page-projects.php
@@ -11,77 +11,112 @@
 <section class="all-projects">
     <div class="container">
         <div class="commercial">
-            <div class="section_title"><h2>Projekty komerycjne</h2></div>
-                <div class="row">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                        <li class="portfolio_card" id="smart">
-                            <figure>
-                                <a href="https://smartelectric.pl/"><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/smart1.png'" alt="I am an example of a work image"></a>
-                            </figure>
-                            <div class="caption">
-                                <a href="https://smartelectric.pl/" target="_blank" title="Smart Electric" aria-describedby="desc-smart"><h3>Smart Electric</h3></a>
-                                <p>Strona interentowa dla firmy Smart Electric</p>
-                                <span class="cta" aria-hidden="true" id="desc-smart">Przejdź na stronę internetową</span>
-                            </div>
-                        </li>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 second-col">
-                        <li class="portfolio_card" id="april">
-                            <figure>
-                                <a href="https://aprildevelopment.pl/"><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/april.png'" alt="April Development"></a>
-                            </figure>
-                            <div class="caption">
-                                <a href="https://aprildevelopment.pl/" target="_blank" title="April Development" aria-describedby="desc-april"><h3>April Development</h3></a>
-                                <p>Strona interentowa dla firmy April Development</p>
-                                <span class="cta" aria-hidden="true" id="desc-april">Przejdź na stronę internetową</span>
-                            </div>
-                        </li>
-                    </div>
-                </div>
+            <div class="section_title"><h2>Projekty komerycjne</h2>
+         <p>* nad którymi pracuje razem z zespołem w firmie Tom&CO z Katowic</p>
+        </div>
 
-                <div class="row second-row">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                        <li class="portfolio_card" id="rehabilitacja">
-                            <figure>
-                                <a href="http://rehabilitacja-kr.pl/"><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/covid(2).png'; ?>" alt="Rehabilitacja Covid"></a>
-                            </figure>
-                            <div class="caption">
-                                <a href="http://rehabilitacja-kr.pl/" target="_blank" title="Rehabilitacja Covid" aria-describedby="desc-rehabilitacja"><h3>Rehabilitacja Covid</h3></a>
-                                <p>Strona interentowa dla firmy oferującej rehabilitację oddechową</p>
-                                <span class="cta" aria-hidden="true" id="desc-rehabilitacja">Przejdź na stronę internetową</span>
-                            </div>
-                        </li>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 second-col">
-                        <li class="portfolio_card" id="momentum">
-                            <figure>
-                                <a href="https://momentumpropertyeducation.com/new"> <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/momentum.png'" alt="I am an example of a work image"></a>
-                            </figure>
-                            <div class="caption">
-                                <a href="https://momentumpropertyeducation.com/new" target="_blank" title="momentum" aria-describedby="desc-momentum"><h3>Momentum Property Education</h3></a>
-                                <p>Nowa strona internetowa wykonana dla Momentum Property Education</p>
-                                <span class="cta" aria-hidden="true" id="desc-momentum">Przejdź na stronę internetową</span>
-                            </div>
-                        </li>
-                    </div>
+        <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                    <li class="portfolio_card" id="agent">
+                        <figure>
+                            <a href="https://www.agentprovocateur.com/"><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/smart1.png'" alt="Agent Provocateur Img"></a>
+                        </figure>
+                        <div class="caption">
+                            <a href="https://www.agentprovocateur.com/" target="_blank" title="Agent Provocateur" aria-describedby="desc-agent"><h3>Agent Provocateur</h3></a>
+                            <p>Strona oraz sklep dla Agent Provocateur</p>
+                            <span class="cta" aria-hidden="true" id="desc-agent">Przejdź na stronę internetową</span>
+                        </div>
+                    </li>
                 </div>
-
-                <div class="row second-row">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 second-col">
-                        <li class="portfolio_card" id="portfolio">
-                            <figure>
-                                <a href="https://platypuswithoutk.github.io/portfolio/"> <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/portfolio1.png'" alt="I am an example of a work image"></a>
-                            </figure>
-                            <div class="caption">
-                                <a href="https://platypuswithoutk.github.io/portfolio/" target="_blank" title="portfolio" aria-describedby="desc-portfolio"><h3>Stare portfolio</h3></a>
-                                <p>Moje dawne portfolio używane do celów rekrutacyjnych w zeszłym roku</p>
-                                <span class="cta" aria-hidden="true" id="desc-portfolio">Przejdź na stronę internetową</span>
-                            </div>
-                        </li>
-                    </div>
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 second-col">
+                    <li class="portfolio_card" id="bonas">
+                        <figure>
+                            <a href="https://www.oliverbonas.com/"><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/april.png'" alt="Oliver Bonas Img"></a>
+                        </figure>
+                        <div class="caption">
+                            <a href="https://www.oliverbonas.com/" target="_blank" title="Oliver Bonast" aria-describedby="desc-bonas"><h3>Oliver Bonas</h3></a>
+                            <p>Strona i sklep interentowy dla firmy Oliver Bonas</p>
+                            <span class="cta" aria-hidden="true" id="desc-bonas">Przejdź na stronę internetową</span>
+                        </div>
+                    </li>
                 </div>
             </div>
-        
+
+
+        <div class="commercial">
+            <div class="section_title"><h2>Projekty komerycjne</h2>
+         <p>* stworzone podczas pracy dla firmy Millenium Studio z Krakowa</p>
+        </div>
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                    <li class="portfolio_card" id="smart">
+                        <figure>
+                            <a href="https://smartelectric.pl/"><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/smart1.png'" alt="I am an example of a work image"></a>
+                        </figure>
+                        <div class="caption">
+                            <a href="https://smartelectric.pl/" target="_blank" title="Smart Electric" aria-describedby="desc-smart"><h3>Smart Electric</h3></a>
+                            <p>Strona interentowa dla firmy Smart Electric</p>
+                            <span class="cta" aria-hidden="true" id="desc-smart">Przejdź na stronę internetową</span>
+                        </div>
+                    </li>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 second-col">
+                    <li class="portfolio_card" id="april">
+                        <figure>
+                            <a href="https://aprildevelopment.pl/"><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/april.png'" alt="April Development"></a>
+                        </figure>
+                        <div class="caption">
+                            <a href="https://aprildevelopment.pl/" target="_blank" title="April Development" aria-describedby="desc-april"><h3>April Development</h3></a>
+                            <p>Strona interentowa dla firmy April Development</p>
+                            <span class="cta" aria-hidden="true" id="desc-april">Przejdź na stronę internetową</span>
+                        </div>
+                    </li>
+                </div>
+            </div>
+
+            <div class="row second-row">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                    <li class="portfolio_card" id="rehabilitacja">
+                        <figure>
+                            <a href="http://rehabilitacja-kr.pl/"><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/covid(2).png'; ?>" alt="Rehabilitacja Covid"></a>
+                        </figure>
+                        <div class="caption">
+                            <a href="http://rehabilitacja-kr.pl/" target="_blank" title="Rehabilitacja Covid" aria-describedby="desc-rehabilitacja"><h3>Rehabilitacja Covid</h3></a>
+                            <p>Strona interentowa dla firmy oferującej rehabilitację oddechową</p>
+                            <span class="cta" aria-hidden="true" id="desc-rehabilitacja">Przejdź na stronę internetową</span>
+                        </div>
+                    </li>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 second-col">
+                    <li class="portfolio_card" id="momentum">
+                        <figure>
+                            <a href="https://momentumpropertyeducation.com/new"> <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/momentum.png'" alt="I am an example of a work image"></a>
+                        </figure>
+                        <div class="caption">
+                            <a href="https://momentumpropertyeducation.com/new" target="_blank" title="momentum" aria-describedby="desc-momentum"><h3>Momentum Property Education</h3></a>
+                            <p>Nowa strona internetowa wykonana dla Momentum Property Education</p>
+                            <span class="cta" aria-hidden="true" id="desc-momentum">Przejdź na stronę internetową</span>
+                        </div>
+                    </li>
+                </div>
+            </div>
+
+            <div class="row second-row">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 second-col">
+                    <li class="portfolio_card" id="portfolio">
+                        <figure>
+                            <a href="https://platypuswithoutk.github.io/portfolio/"> <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/portfolio1.png'" alt="I am an example of a work image"></a>
+                        </figure>
+                        <div class="caption">
+                            <a href="https://platypuswithoutk.github.io/portfolio/" target="_blank" title="portfolio" aria-describedby="desc-portfolio"><h3>Stare portfolio</h3></a>
+                            <p>Moje dawne portfolio używane do celów rekrutacyjnych w zeszłym roku</p>
+                            <span class="cta" aria-hidden="true" id="desc-portfolio">Przejdź na stronę internetową</span>
+                        </div>
+                    </li>
+                </div>
+            </div>
+        </div>
+
 
         <div class="private">
             <div class="section_title no"><h2>Projekty niekomerycjne</h2></div>
@@ -109,7 +144,7 @@
                                 <span class="cta" aria-hidden="true" id="desc-react">Przejdź na stronę internetową</span>
                             </div>
                         </li>
-                    </div>   
+                    </div>
                 </div>
             </div>
         </div>
