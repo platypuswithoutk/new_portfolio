ff4a34b4fdf88ee227cd7cf00443ad84a3bd1c5d
platypuswithoutk <roksana.kurasinska@gmail.com>
2022-05-27 13:04:20 +0200

    add linkedin link

--- wp-content/themes/mytheme/page-contact.php
+++ wp-content/themes/mytheme/page-contact.php
@@ -15,6 +15,7 @@ get_header();
   <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12" id="contact-data">
       <p>Napisz: <a href="mailto:">roksana.dzioba@gmail.com</a></p>
+      <p>Linkedin:  <a href="https://www.linkedin.com/in/roksana-dzioba-268a33156/">Roksana Dzioba</i></a></p>
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/girl.png.'" alt="logo">
     </div>
   </div>
